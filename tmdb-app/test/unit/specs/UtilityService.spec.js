1310ca2eb6fe2e8e3d07e0f535584ce67deae783
ajslim <ajslim@gmail.com>
2018-08-30 20:58:44 +0100

    Fixes Utility Service spec

--- tmdb-app/test/unit/specs/UtilityService.spec.js
+++ tmdb-app/test/unit/specs/UtilityService.spec.js
@@ -3,6 +3,6 @@ import Utility from '@/services/UtilityService';
 describe('UtilityService', () => {
   it('should correctly execute the intersection function', () => {
     expect(Utility.intersection([1, 2], [1, 3])[0]).to.equal([1][0]);
-    expect(Utility.intersection([1, 2], [3, 4]).length()).to.equal(0);
+    expect(Utility.intersection([1, 2], [3, 4]).length).to.equal(0);
   });
 });
